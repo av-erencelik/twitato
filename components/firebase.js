fe0ebf65a92be4c1c0a399340564f3f6a48abfe3
av-erencelik <erenmemo05@gmail.com>
2023-01-15 20:43:14 +0300

    added env variables

--- components/firebase.js
+++ components/firebase.js
@@ -1,13 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBFx-l4I7gfvOQNDkUAexLOG6LLpDpeAuI",
-  authDomain: "twitato-8b510.firebaseapp.com",
-  projectId: "twitato-8b510",
-  storageBucket: "twitato-8b510.appspot.com",
-  messagingSenderId: "702640937304",
-  appId: "1:702640937304:web:d3c665f73cd3bdb375ad2d",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
